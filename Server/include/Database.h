a6783854cc1574c318962f3614018244f9b9cd02
Hugo Miard <hmiard@gmail.com>
2014-04-01 11:34:37 +0200

    01/04/2014
    adding mysql connector

--- Server/include/Database.h
+++ Server/include/Database.h
@@ -1,6 +1,13 @@
 #ifndef DATABASE_H_INCLUDED
 #define DATABASE_H_INCLUDED
 
+#include "mysql_connection.h"
+
+#include "../include/cppconn/driver.h"
+#include "../include/cppconn/exception.h"
+#include "../include/cppconn/resultset.h"
+#include "../include/cppconn/statement.h"
+
 class Database;
 
 class Database{
